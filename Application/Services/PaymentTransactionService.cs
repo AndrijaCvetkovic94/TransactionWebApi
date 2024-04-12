885d31921d4efb05a1262b2e51b4f33d53801d0d
AndrijaCvetkovic94 <andrijacvetkovic9494@gmail.com>
2024-04-12 23:06:29 +0200

    Initial PaymentTransactionService implementation

--- Application/Services/PaymentTransactionService.cs
+++ Application/Services/PaymentTransactionService.cs
@@ -18,8 +18,44 @@ public class PaymentTransactionService : IPaymentTransactionSerivce
         _currencyRepository = currencyRepository;
     }
     
-    public Task<PaymentTransactionResponseDTO> ExecuteTransactionAsync(PaymentTransactionRequestDTO transactionRequestDTO)
+    public async Task<PaymentTransactionResponseDTO> ExecuteTransactionAsync(PaymentTransactionRequestDTO paymentTransactionRequestDTO)
     {
-        throw new NotImplementedException();
+        var user = await _userRepository.GetUserById(paymentTransactionRequestDTO.UserId);
+
+        if(user == null)
+        {
+            throw new Exception("Non exsisting player");
+        }
+
+        Currency currency = await _currencyRepository.GetCurrencyAsync(paymentTransactionRequestDTO.Currency);
+        if(currency == null)
+        {
+            throw new Exception("Invalid currency");    
+        }
+
+        if(paymentTransactionRequestDTO.Amount <= 0)
+        {
+            throw new Exception("Amount value must be bigger than 0 in order to procced with payment transaction");
+        }
+
+        var transaction = new PaymentTransaction
+        {
+            Id = Guid.NewGuid(),
+            Timestamp = DateTime.UtcNow,
+            Amount = paymentTransactionRequestDTO.Amount,
+            TransactionCurrency = currency,
+            TransactionUser = user,
+            Description = $"{paymentTransactionRequestDTO.Amount} is transfered to players bank account"
+        };
+
+        await _transactionRepository.AddTransactionAsync(transaction);
+
+        return new PaymentTransactionResponseDTO
+        {
+            TransactionId = transaction.Id,
+            Status = 0,
+            Description = "Tranaction executed succesfully"
+        };
+
     }
 }
